5a88ee9968f91bec6ae94d6651dba0cb86541564
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-09 23:00:07 +0100

    Changement de l'image du projectile

--- PROJECTILE.py
+++ PROJECTILE.py
@@ -4,5 +4,5 @@ class Projectile(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.velocity = 132
-        self.image = pygame.image.load('assets/index.jpeg')
+        self.image = pygame.image.load('assets/projectile.png')
         self.rect = self.image.get_rect()
