1912fbdd1c2f80c4a271d19889913748933676cf
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-11 21:26:27 +0100

    Maintenant possible de choisir la largeur et hauteur de la plateforme

--- PLATEFORME.py
+++ PLATEFORME.py
@@ -1,7 +1,7 @@
 import pygame
 class Plateforme(pygame.sprite.Sprite):
-    def __init__(self,x,y):
+    def __init__(self,x,y,width,height):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load('assets/plateforme.png'),(800,20))
+        self.image = pygame.transform.scale(pygame.image.load('assets/plateforme.png'),(width,height))
         self.rect = self.image.get_rect()
         self.rect.x,self.rect.y = x,y
