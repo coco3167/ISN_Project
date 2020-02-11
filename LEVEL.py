b894aca5d6422d3e934ddca76ac0fc34567e8fed
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-11 22:08:02 +0100

    Paramétrage pour acceuillir les portes

--- LEVEL.py
+++ LEVEL.py
@@ -1,10 +1,13 @@
 import pygame
 class Level():
-    def __init__(self,width,height,background,number,listePlateforme):
+    def __init__(self,width,height,background,number,listePlateforme,listeDoor):
         self.width = width
         self.height = height
         self.listePlateforme = pygame.sprite.Group()
         for plateforme in listePlateforme:
             self.listePlateforme.add(plateforme)
+        self.listeDoor = pygame.sprite.Group()
+        for door in listeDoor:
+            self.listeDoor.add(door)
         self.background = background
         self.number = number
