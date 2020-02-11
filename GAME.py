bfd68be2bfd0f077653ea9b370c3862d7380f103
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-11 21:25:33 +0100

    Changement d'un nom de variable et enrichissement d'une 2e plateforme pour l'un des niveaux.

--- GAME.py
+++ GAME.py
@@ -10,9 +10,9 @@ class Game():
         pygame.mixer.music.load('assets/Theme.ogg')
         pygame.mixer.music.play(loops = -1)
         #Matrice avec toutes les plateforme selon les niveaux
-        self.matriceLevel = [
-                            (1000,800,None,0,[PLATEFORME.Plateforme(100,680)]),
-                            (1000,800,None,1,[PLATEFORME.Plateforme(0,700)])
+        self.listeLevel = [
+                            (1000,800,None,0,[PLATEFORME.Plateforme(100,680,800,20)]),#Level 0
+                            (1000,800,None,1,[PLATEFORME.Plateforme(0,780,1000,20),PLATEFORME.Plateforme(50,600,900,20)]),#Level 1
                             ]
 
     def changeLevel(self,numberLevel):
@@ -21,6 +21,6 @@ class Game():
             self.allSprites.remove(self.level.listePlateforme)
         except:
             pass
-        self.level = LEVEL.Level(*self.matriceLevel[numberLevel])
+        self.level = LEVEL.Level(*self.listeLevel[numberLevel])
         #Ajout des sprites du niveau aux sprites généraux
         self.allSprites.add(self.level.listePlateforme)
