8b6c36c1e490cee6e449748b8dfe02283cc33231
Corentin Boblet <bobletc@poste.allende.lyc14.ac-caen.fr>
2020-02-11 15:52:46 +0100

    Abstraction du changement de Level

--- LEVEL.py
+++ LEVEL.py
@@ -1,8 +1,10 @@
 import pygame
 class Level():
-    def __init__(self,width,height,background,number):
+    def __init__(self,width,height,background,number,listePlateforme):
         self.width = width
         self.height = height
         self.listePlateforme = pygame.sprite.Group()
+        for plateforme in listePlateforme:
+            self.listePlateforme.add(plateforme)
         self.background = background
         self.number = number
