2007349e25bae5a508695359a2fbe0e6b25acbc2
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-13 21:30:47 +0100

    Changement de façon depositionner les portes

--- DOOR.py
+++ DOOR.py
@@ -1,8 +1,13 @@
 import pygame
 class Door(pygame.sprite.Sprite):
-    def __init__(self,x,y,width,height,destination):
+    def __init__(self,LeftOrRight,y,destination):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load('assets/door.png'),(width,height))
+        self.image = pygame.transform.scale(pygame.image.load('assets/door.png'),(60,60))#20*20
         self.rect = self.image.get_rect()
-        self.rect.x,self.rect.y = x,y
+        if LeftOrRight == 'Left':
+            self.rect.x = 0
+        elif LeftOrRight == 'Right':
+            self.rect.x = 940
+            self.image = pygame.transform.flip(self.image,True,False)
+        self.rect.y = y
         self.destination = destination
