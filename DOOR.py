d1f2e42dfe62fbc1d8f9b48d18d1eb438d4edf58
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-21 13:22:40 +0100

    Supression du sprite devenu inutile est mise en place d'un rrectangle de collision seul

--- DOOR.py
+++ DOOR.py
@@ -1,13 +1,9 @@
 import pygame
-class Door(pygame.sprite.Sprite):
-    def __init__(self,LeftOrRight,y,destination):
-        super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load('assets/door.png'),(60,60))#20*20
-        self.rect = self.image.get_rect()
+class Door():
+    def __init__(self,LeftOrRight,y,width,height,destination):
         if LeftOrRight == 'Left':
-            self.rect.x = 0
+            x = 0
         elif LeftOrRight == 'Right':
-            self.rect.x = 940
-            self.image = pygame.transform.flip(self.image,True,False)
-        self.rect.y = y
+            x = 940
+        self.rect = pygame.Rect(x,y,width,height)
         self.destination = destination
