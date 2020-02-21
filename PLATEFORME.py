6d8a32e02c7c6c5ae3b59b3e6cbb2bfc3d8ea984
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-21 13:23:41 +0100

    Supression du sprite devenu inutile et mis en place d'un rectangle de collision seul

--- PLATEFORME.py
+++ PLATEFORME.py
@@ -2,6 +2,4 @@ import pygame
 class Plateforme(pygame.sprite.Sprite):
     def __init__(self,x,y,width,height):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load('assets/plateforme.png'),(width,height))
-        self.rect = self.image.get_rect()
-        self.rect.x,self.rect.y = x,y
+        self.rect = pygame.Rect(x,y,width,height)
