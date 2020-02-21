986de77c64816d51b1a22833df28e4396707703b
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-21 13:27:54 +0100

    Changement du Ãau fit que Door et Plateforme ne sont plus des Sprites

--- LEVEL.py
+++ LEVEL.py
@@ -3,12 +3,8 @@ class Level():
     def __init__(self,width,height,backgroundAdress,number,listePlateforme,listeDoor):
         self.width = width
         self.height = height
-        self.listePlateforme = pygame.sprite.Group()
-        for plateforme in listePlateforme:
-            self.listePlateforme.add(plateforme)
-        self.listeDoor = pygame.sprite.Group()
-        for door in listeDoor:
-            self.listeDoor.add(door)
+        self.listePlateforme = listePlateforme
+        self.listeDoor = listeDoor
         if backgroundAdress != None:
             self.background = pygame.transform.scale(pygame.image.load(backgroundAdress),(self.width,self.height))
         self.number = number
