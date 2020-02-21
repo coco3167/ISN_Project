c680de401688db3cdec1e5ba21ad073842141f4c
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-21 11:21:05 +0100

    Ajout d'un background pour le premier niveau

--- GAME.py
+++ GAME.py
@@ -11,12 +11,13 @@ class Game():
         pygame.mixer.music.play(loops = -1)
         #Matrice avec toutes les plateforme selon les niveaux
         self.listeLevel = [
-                            (1000,800,None,0,[PLATEFORME.Plateforme(100,680,800,20)],[DOOR.Door('Left',220,1)]),#Level 0
+                            (1600,800,'assets/backgroundLevel0.png',0,[PLATEFORME.Plateforme(100,680,800,20)],[DOOR.Door('Left',220,1)]),#Level 0
                             (1000,800,None,1,[PLATEFORME.Plateforme(0,780,1000,20),PLATEFORME.Plateforme(100,550,800,20)],[DOOR.Door('Right',190,0)]),#Level 1
                           ]
 
     def update(self):
         self.player.update(self.level.listePlateforme)
+        self.level.update(self.player)
         doorCollided = pygame.sprite.spritecollide(self.player,self.level.listeDoor,False)
         if doorCollided != []:
             self.changeLevel(doorCollided[0].destination)
