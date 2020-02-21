2781fe2cfb2ce3b25b5c61fc7ab57f7aa5e368aa
Corentin Boblet <corent1.creully@sfr.fr>
2020-02-21 13:29:13 +0100

    Dessin du background à chaque changement de niveau

--- GAME.py
+++ GAME.py
@@ -11,23 +11,19 @@ class Game():
         pygame.mixer.music.play(loops = -1)
         #Matrice avec toutes les plateforme selon les niveaux
         self.listeLevel = [
-                            (1600,800,'assets/backgroundLevel0.png',0,[PLATEFORME.Plateforme(100,680,800,20)],[DOOR.Door('Left',220,1)]),#Level 0
-                            (1000,800,None,1,[PLATEFORME.Plateforme(0,780,1000,20),PLATEFORME.Plateforme(100,550,800,20)],[DOOR.Door('Right',190,0)]),#Level 1
+                            (1400,700,'assets/backgroundLevel0.png',0,[PLATEFORME.Plateforme(0,650,1400,50),PLATEFORME.Plateforme(0,343,91,323)],[DOOR.Door('Left',0,45,120,1)]),#Level 0
+                            (1400,700,None,1,[PLATEFORME.Plateforme(0,780,1000,20),PLATEFORME.Plateforme(100,550,800,20)],[DOOR.Door('Right',100,45,120,0)]),#Level 1
                           ]
+        
 
-    def update(self):
+    def update(self,screen):
         self.player.update(self.level.listePlateforme)
-        self.level.update(self.player)
-        doorCollided = pygame.sprite.spritecollide(self.player,self.level.listeDoor,False)
-        if doorCollided != []:
-            self.changeLevel(doorCollided[0].destination)
+        numDoorCollided = self.player.rect.collidelist(self.level.listeDoor)
+        if numDoorCollided != -1:
+            doorCollided = self.level.listeDoor[numDoorCollided]
+            self.changeLevel(doorCollided.destination,screen)
 
-    def changeLevel(self,numberLevel):
-        #Retirement des sprites de l'ancien niveau
-        try:
-            self.allSprites.remove(self.level.listePlateforme,self.level.listeDoor)
-        except:
-            pass
+    def changeLevel(self,numberLevel,screen):
         self.level = LEVEL.Level(*self.listeLevel[numberLevel])
-        #Ajout des sprites du niveau aux sprites généraux
-        self.allSprites.add(self.level.listePlateforme,self.level.listeDoor)
+        screen.blit(self.level.background,(0,0))
+        pygame.display.flip()
