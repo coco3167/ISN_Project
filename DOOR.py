ef01235c7e0b1cc510d1ce0edd19cc195755b0d5
Corentin Boblet <corent1.creully@sfr.fr>
2020-03-04 19:05:01 +0100

    Ajout d'une destination pour le joueur quand il emprunte la porte

--- DOOR.py
+++ DOOR.py
@@ -1,9 +1,10 @@
 import pygame
 class Door():
-    def __init__(self,LeftOrRight,y,width,height,destination):
+    def __init__(self,LeftOrRight,y,width,height,destination,coordPlayer):
         if LeftOrRight == 'Left':
             x = 0
         elif LeftOrRight == 'Right':
             x = 1000-width
         self.rect = pygame.Rect(x,y,width,height)
         self.destination = destination
+        self.coordPlayer = coordPlayer
